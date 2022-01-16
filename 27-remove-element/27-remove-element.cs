841b52b8c4586d6fda382aee10966676247335bf
Balázs Tóth <grandmastar27@gmail.com>
2022-01-16 18:44:37 +0100

    Time: 189 ms (22.68%), Space: 39 MB (76.52%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.Length; j++)
+        {
+            if(nums[j] != val)
+            {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
