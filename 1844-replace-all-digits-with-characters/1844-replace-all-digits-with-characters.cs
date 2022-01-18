e1f0719e70396866cca687ac65beb491fc077836
Balázs Tóth <grandmastar27@gmail.com>
2022-01-18 20:48:05 +0100

    Time: 136 ms (14.61%), Space: 35.5 MB (66.29%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cs
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.cs
@@ -3,12 +3,8 @@ public class Solution {
         char[,] sC = new char[s.Length, 2];
         string r = "";
         for (int i = 1; i <= s.Length; i++) {
-            //sC[i, 0] = s[i];
-            //sC[i, 1] = 97 + ((int)s[i]);
-
             if (i < s.Length) {
                 if (((i - 1) % 2 == 0) && (i) % 2 != 0) {
-                    // Console.WriteLine($"{s[i - 1]} {s[i]}");
                     int c = ((int)s[i - 1]) + (Convert.ToInt32(s[i])) - 48;
                     char cC = (char)c;
                     r += s[i - 1].ToString() + cC;
