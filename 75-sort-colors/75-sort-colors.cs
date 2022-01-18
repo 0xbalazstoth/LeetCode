20773f961afca5ebedddc1317544eaee54635e0b
Balázs Tóth <grandmastar27@gmail.com>
2022-01-18 16:39:52 +0100

    Time: 124 ms (64.33%), Space: 40.4 MB (43.49%) - LeetHub

--- 75-sort-colors/75-sort-colors.cs
+++ 75-sort-colors/75-sort-colors.cs
@@ -1,6 +1,9 @@
 public class Solution {
     public void SortColors(int[] nums) {
-        SwapSort(ref nums);
+        // SwapSort(ref nums);
+        // MinimumSort(ref nums);
+        // BubbleSort(ref nums);
+        BetterBubbleSort(ref nums);
     }
     
     public void SwapSort(ref int[] nums) {
@@ -12,6 +15,48 @@ public class Solution {
                     nums[j] = swap;
                 }    
             }
+        }
+    }
+    
+    public void MinimumSort(ref int[] nums) {
+        for (int i = 0; i < nums.Length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < nums.Length; j++) {
+                if (nums[min] > nums[j]) {
+                    min = j;
+                }
+            }
+            int swap = nums[i];
+            nums[i] = nums[min];
+            nums[min] = swap;
+        }
+    }
+    
+    public void BubbleSort(ref int[] nums) {
+        for (int i = nums.Length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int swap = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = swap;
+                }
+            }
+        }
+    }
+    
+    public void BetterBubbleSort(ref int[] nums) {
+        int i = nums.Length - 1;
+        while(i > 0) {
+            int index = -1;
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int swap = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = swap;
+                    index = j;
+                }
+            }
+            i = index;
         }
     }
 }
